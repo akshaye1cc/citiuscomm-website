c307a01e5c1a6277fcd5b439f040f124605871ac
Shalini Pandey <shalinipandey1207@gmail.com>
2026-04-01 14:59:10 +0530

    implemented About Us 'Who We Are' section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,9 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Breadcrumb from "@/components/Common/Breadcrumb";
-
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "About Page | Free Next.js Template for Startup and SaaS",
-  description: "This is About Page for Startup Nextjs Template",
-  // other metadata
-};
+import WhoWeAre from "@/components/About/WhoWeAre";
 
 const AboutPage = () => {
   return (
     <>
-      <Breadcrumb
-        pageName="About Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
-      />
-      <AboutSectionOne />
-      <AboutSectionTwo />
+      <WhoWeAre />        
     </>
   );
 };
