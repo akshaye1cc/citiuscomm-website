a0177ce520eac1891f149917ab6d82baeb0bf977
Akshaye Aaron Azariah <140735998+akxhayx@users.noreply.github.com>
2026-04-06 15:55:08 +0530

    contact component deleted & contact page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Testimonials from "@/components/Testimonials";
@@ -17,7 +16,6 @@ export default function Home() {
       <AboutSectionOne />
       <AboutSectionTwo />
       <Testimonials />
-      <Contact />
     </>
   );
 }
